1a9d3fb20835c2cbaefcee94da712384a0eaaaea
GabrielStancu <gabriel.stancu07@yahoo.com>
2023-01-04 22:48:31 +0200

    Added CORS policy

--- RedisCart/Program.cs
+++ RedisCart/Program.cs
@@ -10,6 +10,13 @@ builder.Services.AddStackExchangeRedisCache(opt => {
     opt.Configuration = builder.Configuration.GetConnectionString("RedisCart");
     opt.InstanceName = "ShopAppCart_";
 });
+builder.Services.AddCors(opt => {
+    opt.AddPolicy(name: "AllowOrigins", builder => {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 builder.Services.AddServices();
 
 var app = builder.Build();
@@ -17,6 +24,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.UseCors("AllowOrigins");
 app.UseAuthorization();
 app.MapControllers();
 
